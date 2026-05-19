66834e33381feb45ef1cc601ed22c4d8eaba961e
Victor Njoroge <victor.njoroge@sonarsource.com>
2026-05-19 10:18:24 -0500

    Add index.js with intentional code issues

---
+++ index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Issue: Unused variable
+    var unusedVariable = 42;
+
+    // Issue: Empty catch block (Code Smell)
+    try {
+        let x = 10 / 0;
+    } catch (e) {
+        // Left intentionally empty
+    }
+}
